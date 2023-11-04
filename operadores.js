b309671cc2973c2bd11370e36b92c65a30d85366
Brachu <braian.ferrari.bf@gmail.com>
2023-11-04 20:32:53 -0300

    adicione los operadores de js

---
+++ operadores.js
@@ -0,0 +1,5 @@
+function estudiante(){
+    let valor1 = document.getElementById('valor1').value;
+    let valor2 = document.getElementById('valor2').value;
+    let valor3 = document.getElementById('valor3').value;
+}
