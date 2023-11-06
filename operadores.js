ca39dad5a7fa3354f2da439579be7cc7e751e9d0
Brachu <braian.ferrari.bf@gmail.com>
2023-11-06 00:01:21 -0300

    idente el codigo

--- operadores.js
+++ operadores.js
@@ -1,46 +1,26 @@
-function getValorById(id){
-     return document.getElementById(id).value;
-    
-}
-
-function calcular(){
-    let valor1 = Number(getValorById('valor1'));
-    let valor2 =Number(getValorById('valor2'));
-    let valor3 = Number(getValorById('valor3'));
-
-    if (categoria === 'estudiante') {
-        descuento = 0.8; // 80% de descuento
-      } else if (categoria === 'trainee') {
-        descuento = 0.5; // 50% de descuento
-      } else if (categoria === 'junior') {
-        descuento = 0.15; // 15% de descuento
-      }
-}
-
-
 function calcularPrecioTotal(cantidadTickets, categoria) {
-  const precioPorTicket = 200;
+    const precioPorTicket = 200;
 
-  let descuento = 0;
+    let descuento = 0;
 
-  if (categoria === '1') {
-    descuento = 0.8; // 80% de descuento para Estudiante
-  } else if (categoria === '2') {
-    descuento = 0.5; // 50% de descuento para Trainee
-  } else if (categoria === '3') {
-    descuento = 0.15; // 15% de descuento para Junior
-  }
+    if (categoria === '1') {
+        descuento = 0.8; 
+    } else if (categoria === '2') {
+        descuento = 0.5; 
+    } else if (categoria === '3') {
+        descuento = 0.15; 
+    }
 
-  const precioTotal = cantidadTickets * precioPorTicket * (1 - descuento);
+    const precioTotal = cantidadTickets * precioPorTicket * (1 - descuento);
 
-  return precioTotal;
+    return precioTotal;
 }
 
 function calcular() {
-  const cantidadTickets = document.querySelector('.inputCant').value;
-  const categoria = document.querySelector('.select').value;
+    const cantidadTickets = document.querySelector('.inputCant').value;
+    const categoria = document.querySelector('.select').value;
 
-  const precioFinal = calcularPrecioTotal(cantidadTickets, categoria);
-  document.querySelector('#resultado').textContent = `Total a Pagar: $${precioFinal}`;
+    const precioFinal = calcularPrecioTotal(cantidadTickets, categoria);
+    document.querySelector('#resultado').textContent = `Total a Pagar: $${precioFinal}`;
 }
 
